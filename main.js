33ec2f86826f422f61867c6224cc3090ec51ef71
leticia almeida <23143052@edu119.local>
2023-08-02 13:17:59 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+/*variaveis */
+
+/*atvd1*/
+var favoriteFood= 'pizza';
+
+/*atvd2*/
+var numOfSlices=8
+
+/*atvd3*/
+console.log(favoriteFood)
+
+/*atvd4*/
+console.log(numOfSlices)
+
+/*atvd5*/
+/*diferênça entre var e let: var é global, já a variavel let é 
+limitada e pode ser usada apenas dentro de seu escopo*/
+
+/*atvd6*/
+let changeMe = true;
+
+/*atvd7*/
+changeMe =false;
+
+/*atvd8*/
+console.log(changeMe)
+
+/*atvd9*/
+const entree='enchiladas';
+
+/*atvd10*/
+console.log(entree)
+
+/*atvd11*/
+/*entree='noite';*/
+
+/*atvd12*/
+
+let levelup=15
+levelup+=5
+
+/*atvd13*/
+let powerlevel=320
+powerlevel-=100
+
+/*atvd14*/
+let multiplyme=20
+multiplyme=11
+
+/*atvd15*/
+let quarterMe=50
+quarterMe/=4
+
+/*atvd16&*/
+let gainedDollar=10
+gainedDollar++;
+
+/*atvd17*/
+let lostDollar=6
+lostDollar--;
+
+/*ayvd18*/
+let MyName='leehalo'
+let myCity='Osasco'
+console.log (`my is ${MyName}. My favorite city is ${myCity}`)
+//atvd19
+const newVariable= 42;
+console.log(typeof newVariable);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
